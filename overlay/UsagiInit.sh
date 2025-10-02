c489b0bbd943d5b265c66d7de43fb8a01288e0b0
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-02 09:42:19 -0500

    FEAT: auto-restart nginx every 23 hours

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,4 +1,4 @@
 #!/UsagiInit
 mkdir -p /tmp/nginx/logs
 v2ray run -config /etc/v2ray/config.json > /dev/null &
-nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+timeout 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
