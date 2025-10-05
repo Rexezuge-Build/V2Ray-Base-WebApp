788240786c86e0ae9decef6d58eceb4ed2ee083e
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-05 07:20:35 -0500

    FEAT: do not auto restart nginx

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -2,4 +2,4 @@
 mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
 v2ray run -config /etc/v2ray/config.json > /dev/null &
-timeout 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
