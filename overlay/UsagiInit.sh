024e9ae8243d6aab49353f35ec59b85404b3af7f
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-15 23:00:58 -0500

    FEAT: disable auto restart every 23 hours

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,5 +1,5 @@
 #!/UsagiInit
 mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
-/bin/timeout -s KILL 23h v2ray run -config /etc/v2ray/config.json > /dev/null &
-/bin/timeout -s KILL 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+v2ray run -config /etc/v2ray/config.json > /dev/null &
+nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
