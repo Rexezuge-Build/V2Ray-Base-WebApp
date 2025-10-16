abcf0b375b3b304b048098e9b02b83a4c3fc5659
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-16 09:13:12 -0500

    FIX: send default signal on timeout instead of sigkill

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -2,5 +2,5 @@
 mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN &
-/bin/timeout -s KILL 23h v2ray run -config /etc/v2ray/config.json > /dev/null &
-/bin/timeout -s KILL 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+/bin/timeout 23h v2ray run -config /etc/v2ray/config.json > /dev/null &
+/bin/timeout 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
