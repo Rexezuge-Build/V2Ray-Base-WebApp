a96543a75c866a15fc85c9c6cc1f868014acc734
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-16 10:12:28 -0500

    FEAT: hide logs from cloudflared

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,6 +1,6 @@
 #!/UsagiInit
 mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
-/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN &
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
 /bin/timeout 23h v2ray run -config /etc/v2ray/config.json > /dev/null &
 /bin/timeout 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
