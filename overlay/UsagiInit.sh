548d87d68a23f225a4888c3b448a97984da33462
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-18 20:30:24 -0500

    FIX: keep nginx running

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -3,4 +3,4 @@ mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
 /bin/timeout 23h v2ray run -config /etc/v2ray/config.json > /dev/null &
-/bin/timeout 23h nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
