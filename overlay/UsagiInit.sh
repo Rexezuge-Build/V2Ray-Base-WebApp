e533f40c0867d8ce4da31ab4ce2ebd7b19888f89
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-11-03 22:48:18 +0000

    FIX: specify the full path of executables

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -2,5 +2,5 @@
 mkdir -p /tmp/nginx/logs
 /NginxUptime-Go > /dev/null &
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
-v2ray run -config /etc/v2ray/config.json > /dev/null &
-nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
+/usr/local/bin/v2ray run -config /etc/v2ray/config.json > /dev/null &
+/usr/sbin/nginx -p /tmp/nginx -c /etc/nginx/nginx.conf -g "daemon off;" > /dev/null &
